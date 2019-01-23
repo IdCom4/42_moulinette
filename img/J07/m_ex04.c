fec52a9639c448c4d80b763e060e579ac0a8ead0
Idriss Cornuau <cornuau.idriss@gmail.com>
2019-01-23 11:31:40 +0100

    ajout j07

--- img/J07/m_ex04.c
+++ img/J07/m_ex04.c
@@ -30,50 +30,64 @@ int main(void)
 	str = strdup("coucou");
 	tab = ft_split_whitespaces(str);
 	size = print_tab(tab);
-	if (size != 1 || strcmp(str, "coucou") != 0)
-		printf("KO\n");
+	if (size != 1)
+	   printf(" size = %d KO\n", size);
+	if (strcmp(str, "coucou") != 0)
+		printf("cmp KO\n");
 	free(str);
 
 	str = strdup("coucou les gens\ncomment\tallez vous ?");
 	tab = ft_split_whitespaces(str);
 	size = print_tab(tab);
-	if (size != 7 || strcmp(str, "coucou les gens\ncomment\tallez vous ?") != 0)
-		printf("KO\n");
+	if (size != 7)
+	   printf(" size = %d KO\n", size);
+	if (strcmp(str, "coucou les gens\ncomment\tallez vous ?") != 0)
+		printf("cmp KO\n");
 	free(str);
 
 	str = strdup("coucou vous");
 	tab = ft_split_whitespaces(str);
 	size = print_tab(tab);
-	if (size != 2 || strcmp(str, "coucou vous") != 0)
-		printf("KO\n");
+	if (size != 2)
+	   printf(" size = %d KO\n", size);
+	if (strcmp(str, "coucou vous") != 0)
+		printf("cmp KO\n");
 	free(str);
 
 	str = strdup("salut,ca\tva\tbien?");
 	tab = ft_split_whitespaces(str);
 	size = print_tab(tab);
-	if (size != 3 || strcmp(str, "salut,ca\tva\tbien?") != 0)
-		printf("KO\n");
+	if (size != 3)
+	   printf(" size = %d KO\n", size);
+	if (strcmp(str, "salut,ca\tva\tbien?") != 0)
+		printf("cmp KO\n");
 	free(str);
 
 	str = strdup("   \t\n  \t salut\t   \n\n\n\t");
 	tab = ft_split_whitespaces(str);
 	size = print_tab(tab);
-	if (size != 1 || strcmp(str, "   \t\n  \t salut\t   \n\n\n\t") != 0)
-		printf("KO\n");
+	if (size != 1)
+	   printf(" size = %d KO\n", size);
+	if (strcmp(str, "   \t\n  \t salut\t   \n\n\n\t") != 0)
+		printf("cmp KO\n");
 	free(str);
 
 	str = strdup("\t\t\n\n   \t\n  \t");
 	tab = ft_split_whitespaces(str);
 	size = print_tab(tab);
-	if (size != 0 || strcmp(str, "\t\t\n\n   \t\n  \t") != 0)
-		printf("KO\n");
+	if (size != 0)
+	   printf("size = %d KO\n", size);
+	if (strcmp(str, "\t\t\n\n   \t\n  \t") != 0)
+		printf("cmp KO\n");
 	free(str);
 
 	str = strdup("\t\t\n    \t\n \t \n coucou \t  \n\n \t le\t\n  monde    \t  \n\n \t");
 	tab = ft_split_whitespaces(str);
 	size = print_tab(tab);
-	if (size != 3 || strcmp(str, "\t\t\n    \t\n \t \n coucou \t  \n\n \t le\t\n  monde    \t  \n\n \t") != 0)
-		printf("KO\n");
+	if (size != 3)
+	   printf("size = %d KO\n", size);
+	if (strcmp(str, "\t\t\n    \t\n \t \n coucou \t  \n\n \t le\t\n  monde    \t  \n\n \t") != 0)
+		printf("cmp KO\n");
 	free(str);
 
 	return (0);
--- img/J07/m_ex06.c
+++ img/J07/m_ex06.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 char *ft_convert_base(char *nbr, char *base_from, char *base_to);
 
